6f2ebacc1f677942d4a73b43dc3375b6dd25ad3e
Garrett Hill <gahill2018@protonmail.com>
2022-04-15 16:44:30 -0400

    switching to desktop editing

--- tpbt/src/Examples.hs
+++ tpbt/src/Examples.hs
@@ -7,40 +7,80 @@ Spring 2022
 -}
 
 {-# LANGUAGE TemplateHaskell   #-}
-{-# LANGUAGE DeriveFoldable    #-}
-{-# LANGUAGE DeriveFunctor     #-}
-{-# LANGUAGE DeriveTraversable #-}
-
 
 module Examples where
 
 import TargetedPBT
-import QuickCheck
+import Test.QuickCheck (
+  Arbitrary,
+  Property,
+  arbitrary,
+  choose,
+  counterexample,
+  elements,
+  listOf,
+  quickCheck,
+  sample,
+  vectorOf
+  )
+import qualified Test.QuickCheck as QC
 import Control.Lens
 
-data (Wordle g) = MkResult {
-    _curr_g      :: [g],
-    _greens      :: [(g, Int)], -- items we know the position of
-    _yellows     :: [g],        -- items we don't know the place of
-    _greys       :: [g],        -- items not in the list
+data Wordle = Wordle {
+    _curr_g      :: [Char],        -- what we want to guess next
+    _greens      :: [(Char, Int)], -- items we know the position of
+    _yellows     :: [Char],        -- items we don't know the place of
+    _greys       :: [Char],        -- items not in the list
     _turns_remn  :: Int         -- turns remaining
-}
+} deriving (Eq, Show)
 
-data (Game g) = MkHistory {
-    _wordle      :: (Wordle g),
-    _key         :: [g]
+data Game = Game {
+    _wordle      :: Wordle, -- the wordle trying to guess the key
+    _key         :: [Char]  -- the key the wordle is trying to guess
 }
 
 makeLenses ''Wordle
 makeLenses ''Game
 
+wordle_size = 5
+
 instance Arbitrary Wordle where
-  arbitrary = Turn {
-    _guess = (arbitrary :: [Char]),
-    _green = [],
-    _yellow = [],
-    _grey = []
-    }
+  arbitrary = QC.elements [Wordle {
+     _curr_g = gen_str wordle_size   -- change to random str of len wordle_size
+    ,_greens = []        :: [(Char, Int)]  -- nothing known
+    ,_yellows = []       :: [Char]         -- ^^^^^^^^^^^^^
+    ,_greys = []         :: [Char]         -- ^^^^^^^^^^^^^
+    ,_turns_remn = 5                 -- this may need to change later
+    }]
+  shrink = undefined
+
+gen_str :: Int -> String
+gen_str i = ""
+
+
+prop_validWordle :: Wordle -> Property
+prop_validWordle (Wordle c_g grn yel gry trn)
+  | not (c_g_valid c_g) = counterexample ("c_g invalid: " ++ c_g) False
+  | not (grn_valid grn) = counterexample ("grn invalid: " ++ show (grn)) False
+  | not (yel_valid yel) = counterexample ("yel invalid: " ++ yel) False
+  | not (gry_valid gry) = counterexample ("gry invalid: " ++ gry) False
+  | not (trn_valid trn) = counterexample ("trn invalid: " ++ (show trn)) False
+  | otherwise = counterexample "" True
+
+c_g_valid :: [Char] -> Bool
+c_g_valid c_g = length c_g == wordle_size
+
+grn_valid :: [(Char, Int)] -> Bool
+grn_valid grn = length grn <= wordle_size
+
+yel_valid :: [Char] -> Bool
+yel_valid yel = length yel <= wordle_size
+
+gry_valid :: [Char] -> Bool
+gry_valid gry = length gry <= 26 - wordle_size
+
+trn_valid :: Int -> Bool
+trn_valid trn = True
 
 
 
