722104b2b4405505c1eebd40f3042fbd02d08de9
Garrett Hill <gahill2018@protonmail.com>
2022-04-29 22:13:02 -0400

    demonstration of efficiency difference between prop and tqc testing completed

--- tpbt/src/Wordle.hs
+++ tpbt/src/Wordle.hs
@@ -55,28 +55,50 @@ data Veil = V {
   , turns     :: Int
   } deriving (Show, Read, Eq, Ord)
 
-{- Default Testing Wordle -}
+{- Default Wordle -}
 def_w :: Wordle
 def_w = (W [] w_pool)
 
-{- Default Testing Veil -}
-def_v :: Veil
-def_v = (V def_w "cabal" 5)
-
+{-
+Generating "complex" arbitrary wordles is impossible due to not being
+able to make any guesses
+-}
 instance Arbitrary Wordle where
-  arbitrary = W <$> (return [])  <*> (return w_pool)
-  shrink (W (g:gs) wp) = [(W gs wp)]
-  shrink w = []
+  arbitrary = return def_w
+  shrink (W (g:gs) wp) = []
 
+prop_wValid :: Wordle -> Bool
+prop_wValid w@(W gs wp) = length gs <= 5 
+
+{-
+Veils will be the primary testing datatype as they can actually take
+turns
+-}
 instance Arbitrary Veil where
-  arbitrary = V <$> arbitrary <*> elements w_pool <*> return 5
-  shrink (V w k t) = []
+  arbitrary = do
+      init@(V w k t) <- V <$> (return def_w) <*> elements w_pool <*> return 5
+      turns <- elements [0 .. 5]
+      -- get list of non-evaled functions that would pick from w_pool
+      arb_turns init turns
+  shrink _ = []
+
+arb_turns :: Veil -> Int -> Gen Veil
+arb_turns v@(V w@(W gs wp) k t) i
+  | solveable v = return v
+  | i > 0 = do
+      new_guess <- elements wp
+      arb_turns (veilTurn new_guess v) (i-1)
+  | otherwise = return v
+
+prop_vValid :: Veil -> Bool
+prop_vValid v@(V w k t) = (length k == 5) && (t <= 5) && prop_wValid w
 
 
 ---------------- FUNCTIONS ------------------
 
 veilTurns :: Int -> [String] -> Veil -> Veil
 veilTurns i (s:ss) v@(V w@(W gs wp) k t)
+  | solveable v = v
   | i == 1 = veilTurn s v
   | i >= 2 = veilTurns (i-1) ss (veilTurn s v)
 veilTurns i ss v = v
@@ -84,6 +106,7 @@ veilTurns i ss v = v
 {- Pass a string to a veil to guess, and returned the updated veil -}
 veilTurn :: String -> Veil -> Veil
 veilTurn s v@(V w@(W gs wp) k t)
+  | solveable v = v
   | t >= 1 = let new_g = guess s k
                  new_gs = new_g : gs
              in
@@ -101,10 +124,13 @@ guess gWord tWord = foldl f [] $ zip gWord tWord
       where
         guessType
           | g == t = CorrectSpot
-          | targetTotal g - correctTotal g - wrongBefore g > 0 = WrongSpot
+          | elem g tWord = WrongSpot
           | otherwise = NotInWord
+          
         wrongBefore c = length . filter (== c) . map fst . filter ((== WrongSpot) . snd) $ acc
+        
         targetTotal c = length . filter (== c) $ tWord
+        
         correctTotal c = length . filter (\(a, b) -> a == c && b == c) $ zip gWord tWord
 
 {-
@@ -114,29 +140,36 @@ word pool that violate the information contained in the guesses
 trimPool :: [Guess] -> [String] -> [String]
 trimPool gs wp = [s | s <- wp, not (s `violatesList` gs)]
 
+{- Checks if a given string violates any of the guesses -}
 violatesList :: String -> [Guess] -> Bool
 violatesList s [] = False
 violatesList s (g:gs) = violates s s g || violatesList s gs
 
+{-
+Checks if a given string violates a single guess
+-}
 violates :: String -> [Char] -> Guess -> Bool
-violates whole_s s@(c:cs) ((gc,gt):gs) = case gt of
-    Default -> violates whole_s cs gs
+violates whole_s rem_s@(c:cs) ((gc,gt):gs) = case gt of
+    Default -> True
     NotInWord -> elem gc whole_s || violates whole_s cs gs
-    WrongSpot -> (not $ elem c cs) || c == gc || violates whole_s cs gs
+    WrongSpot -> (not $ elem c whole_s) || c == gc || violates whole_s cs gs
     CorrectSpot -> not $ c == gc || violates whole_s cs gs
-violates whole_s []        []          = False
+violates _       []        []          = False
 violates _       _         _           = True
 
-
------------------- TARGETED PBT --------------------
 
 {-
 Any wordle should not have any words in the pool that would violate past
 guesses
+The actual key should also always be in the pool
 -}
-prop_poolValid :: Wordle -> Property
-prop_poolValid w@(W gs wp) = counterexample "poolValid" $
-  not $ or $ map (\s -> violatesList s gs) wp
+prop_poolValid :: Veil -> Property
+prop_poolValid v@(V w@(W gs wp) k t) =
+  counterexample "invalid word in pool" $
+  (not $ any (\word -> violatesList word gs) wp) &&
+  (elem k wp)
+
+------------------ TARGETED PBT --------------------
 
 {-
 For a given wordle, produces the list of all Wordles that have had one
@@ -164,17 +197,50 @@ w_uv :: Wordle -> Int
 w_uv (W gs wp) = (length wp) ^ 2
 
 {-
+The utility value of a veil is just the utility value of the contained
+Wordle
 -}
 v_uv :: Veil -> Int
 v_uv v@(V w@(W gs wp) k t) = w_uv w
 
 {-
-For all x such that x is a member of w_pool,
-there exists a series of five or less guesses to
-arrive at that answer
+returns if a given veil is solveable in i turns
+-}
+solveableIn :: Int -> Veil -> Bool
+solveableIn i v@(V w@(W gs wp) k t)
+  | i > 1  = any (solveableIn (i-1)) (v_nb v)
+  | otherwise = solveable v
+
+{-
+You have successfully narrowed down the possible words to a defined
+risk range
 -}
 solveable :: Veil -> Bool
-solveable v@(V w@(W gs wp) k t) =
-  length wp == 1 ||
-  any solveable (v_nb v)
+solveable v@(V w@(W gs wp) k t) = length wp <= acceptableRisk
+
+{-
+how small our pool needs to be for us to consider randomly guessing as a valid
+solution to the Wordle
+for acceptableRisk = n, % chance of guessing correctly is (1/n) * 100
+    acceptableRisk = 5, % chance of guessing correctly is (1/5) * 100 = 20%
+-}
+acceptableRisk = 5
   
+
+{-
+Compare the speed difference between quickCheckig (solveableIn 5) and
+minUVUnder acceptableRisk
+
+Both should produce counterexamples for the same property, but tqc should be
+significantly faster
+
+On my machine,
+tqc_solveable takes (0.14 secs, 25,552,648 bytes) to find a counterexample
+prop_solveableIn takes (101.87 secs, 44,426,540,840 bytes)
+-}
+
+prop_solveableIn :: IO ()
+prop_solveableIn = quickCheck (counterexample "solveable" $ solveableIn 5)
+
+tqc_solveableIn :: IO ()
+tqc_solveableIn = minUVUnder v_uv v_nb acceptableRisk 5
